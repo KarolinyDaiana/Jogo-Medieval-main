3552737a7be7f36f88875c47ebca54b7e79e5ca7
KarolinyDaiana <karoliny.cieply@gmail.com>
2023-08-14 16:11:40 -0300

    dia 14.08

---
+++ script.js
@@ -0,0 +1,9 @@
+const glossarioBotao = document.getElementById("ver-glossario");
+const glossarioLista = document.getElementById("glossario");
+
+glossarioBotao.addEventListener('click', function() {
+
+    glossarioLista.style.display = 'block';
+    console.log("Olá");
+
+});
