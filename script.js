fffa64402c3b228936d6510e5b9f86ff1d7792ea
Karoliny Daiana Cieply <128431288+KarolinyDaiana@users.noreply.github.com>
2023-09-03 11:23:19 -0300

    03.09

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const glossarioBotao = document.getElementById("ver-glossario");
 const glossarioLista = document.getElementById("glossario");
 
 glossarioBotao.addEventListener('click', function() {
-
     glossarioLista.style.display = 'block';
-    console.log("Olá");
-
 });
+
+const esconder = document.getElementById("esconder");
+
+esconder.addEventListener('click', function() {
+    glossarioLista.style.display = 'none';
+})
